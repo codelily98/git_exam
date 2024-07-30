869d2a64a41270091074c01051e1ca626089b70f
codelily <coldelily98@naver.com>
2024-07-30 15:55:29 +0900

    update 반갑습니다

--- src/exam/HelloTest.java
+++ src/exam/HelloTest.java
@@ -2,6 +2,6 @@ package exam;
 
 public class HelloTest {
 	public static void main(String[] args) {
-		System.out.println("안녕하세요 Git Test입니다. - 1");
+		System.out.println("반갑습니다 Git Test입니다. - 1");
 	}
 }
